655fe52fd309aa5bebe0c1aea9e9e4b30d06723a
magnusfb <magnusfbj@gmail.com>
2019-04-25 12:17:54 +0200

    WEighted Grading

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -14,10 +14,12 @@ namespace GradeBook.GradeBooks
         public string Name { get; set; }
         public List<Student> Students { get; set; }
         public GradeBookType Type { get; set; }
+        public bool IsWeighted { get; set; }
 
-        public BaseGradeBook(string name)
+        public BaseGradeBook(string name, bool isWeighted)
         {
             Name = name;
+            IsWeighted = isWeighted;
             Students = new List<Student>();
         }
 
@@ -107,20 +109,34 @@ namespace GradeBook.GradeBooks
 
         public virtual double GetGPA(char letterGrade, StudentType studentType)
         {
+            double toRet = 0;
             switch (letterGrade)
             {
                 case 'A':
-                    return 4;
+                    toRet = 4;
+                    break;
                 case 'B':
-                    return 3;
+                    toRet = 3;
+                    break;
                 case 'C':
-                    return 2;
+                    toRet = 2;
+                    break;
                 case 'D':
-                    return 1;
+                    toRet = 1;
+                    break;
                 case 'F':
-                    return 0;
+                    toRet = 0;
+                    break;
             }
-            return 0;
+
+            if (IsWeighted)
+            {
+                if (studentType == StudentType.DualEnrolled || studentType == StudentType.Honors)
+                    toRet++;
+            }
+
+
+            return toRet;
         }
 
         public virtual void CalculateStatistics()
